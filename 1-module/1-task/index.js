79d72eebdd1e76efe10f74040567d30261b4cb21
Egor Vladimirov <wladimirow.es@gamil.com>
2021-02-08 13:45:14 +0300

    factorial function completed

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,13 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+
+  let result = n;
+  for (let index = n - 1; index > 0; index--) {
+    result *= (n - index);
+  }
+  return result;
 }
