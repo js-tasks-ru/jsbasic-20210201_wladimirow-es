262dd9034b5e0091bbbc3645604373572bfd4e9c
Egor Vladimirov <wladimirow.es@gamil.com>
2021-02-18 13:20:47 +0300

    getMinMax completed

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = {};
+  let separetedArray = str.split(',').join(' ').split(' ');
+  let numbers = separetedArray.filter(value => Number.isFinite(+value));
+  
+  result.max = Math.max(...numbers);
+  result.min = Math.min(...numbers);
+  
+  return result;
 }
