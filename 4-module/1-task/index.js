d36646151ffcfe116b001e7e6db693b7ab41fcef
Egor Vladimirov <wladimirow.es@gamil.com>
2021-02-18 14:58:28 +0300

    makeFriendList completed

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,7 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let result = document.createElement('ul');
+  let friendList = friends.map(friend => '<li>' + friend.firstName + ' ' + friend.lastName + '</li>');
+  result.insertAdjacentHTML("afterbegin", friendList.join(''));
+
+  return result;
 }
